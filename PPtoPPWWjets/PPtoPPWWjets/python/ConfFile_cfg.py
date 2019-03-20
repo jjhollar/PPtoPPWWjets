73186903b0595bf7519a0fd5d0efab67432be303
Jonathan <jjhollar@cern.ch>
2019-03-20 09:00:04 +0100

    Jet updates from Ksenia

--- PPtoPPWWjets/PPtoPPWWjets/python/ConfFile_cfg.py
+++ PPtoPPWWjets/PPtoPPWWjets/python/ConfFile_cfg.py
@@ -8,7 +8,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = ''
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -16,6 +16,17 @@ process.source = cms.Source("PoolSource",
 #'file:/tmp/jjhollar/50D30A02-FB08-E811-8D0B-44A842CFD5D8.root'
 
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_1.root',
+<<<<<<< HEAD
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_2.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_3.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_4.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_5.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_6.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_7.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_8.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_9.root',
+#'/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_10.root'
+=======
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_2.root',
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_3.root',
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_4.root',
@@ -25,6 +36,7 @@ process.source = cms.Source("PoolSource",
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_8.root',
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_9.root',
 '/store/user/kshcheli/ExclWWHadronic2017Analysis/MCv2/fpmc_exclww/step3_fpmc_exclww_a0W2point5e-6_alldecays_xi1to30pct_miniaodv2_10.root'
+>>>>>>> 535c935a54c61db271a5a935bdba13963aa11c57
 
 #'file:/tmp/jjhollar/7402ADA9-C609-E811-BCFE-C0BFC0E5686E.root'
 
@@ -77,6 +89,7 @@ YEAR=2017
 ERA="F"
 
 if MC:
+    DOSMEARING=True
     if YEAR==2016:
         process.GlobalTag.globaltag = '94X_mcRun2_asymptotic_v3'
         process.load("PPtoPPWWjets.PPtoPPWWjets.HLTFilter2016_cfi")
@@ -86,6 +99,7 @@ if MC:
         process.load("PPtoPPWWjets.PPtoPPWWjets.HLTFilter_cfi")
         process.hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
 else:
+    DOSMEARING=False
     if YEAR == 2016:
         process.GlobalTag.globaltag = '94X_dataRun2_v10'
         process.load("PPtoPPWWjets.PPtoPPWWjets.HLTFilter2016_cfi")
@@ -94,6 +108,8 @@ else:
         process.GlobalTag.globaltag ='94X_dataRun2_v11'
         process.load("PPtoPPWWjets.PPtoPPWWjets.HLTFilter_cfi")
         process.hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
+<<<<<<< HEAD
+=======
 
 
 # JH - modified Dec 10, 2018
@@ -169,40 +185,149 @@ if YEAR == 2017:
                 JETCorrPayload = 'AK8PFchs', JETCorrLevels = ['L1FastJet', 'L2Relative', 'L3Absolute']
                 )
 
+>>>>>>> 535c935a54c61db271a5a935bdba13963aa11c57
 
 
-# New from Ksenia+Finn - update jet corrections
-from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
-updateJetCollection(
-   process,
-   jetSource = cms.InputTag('slimmedJetsAK8'),
-   labelName = 'UpdatedJECAK8',
-   jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'NONE')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
-)
+# JH - modified Dec 10, 2018
+# JEC, if using from sqlite file instead of global tag. 
+############################### JEC #####################
+#### Load from a sqlite db, if not read from the global tag
+#process.load("CondCore.DBCommon.CondDBCommon_cfi")
+#from CondCore.DBCommon.CondDBSetup_cfi import *
+#
+#if MC == True:
+#    connectString = cms.string('sqlite:JEC2017/Fall17_17Nov2017_V32_94X_MC.db')
+#    #JetCorrectorParametersCollection_Fall17_17Nov2017_V32_94X_MC_AK4PFPuppi                                                                               
+#    tagName = 'Fall17_17Nov2017_V32_94X_MC_AK4PFchs'
+#    tagNamePuppi = 'Fall17_17Nov2017_V32_94X_MC_AK4PFPuppi'
+#else:
+#    connectString = cms.string('sqlite:JEC2017/Fall17_17Nov2017_V32_94X_DATA.db')
+#    # B C DE F
+#    tagName = 'Fall17_17Nov2017_V32_94X_DATA_AK4PFchs'
+#    tagNamePuppi = 'Fall17_17Nov2017_V32_94X_DATA_AK4PFPuppi'
+#
+#data only, mc hard coded. Need to be fixed per Run
+#
+#
+#process.jec = cms.ESSource("PoolDBESSource",
+#      DBParameters = cms.PSet(
+#        messageLevel = cms.untracked.int32(0)
+#        ),
+#      timetype = cms.string('runnumber'),
+#      toGet = cms.VPSet(
+#      cms.PSet(
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_%s'%tagName),
+#            label  = cms.untracked.string('AK4PFchs')
+#            ),
+#      cms.PSet( ## AK8
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_%s'%re.sub('AK4','AK8',tagName)),
+#            label  = cms.untracked.string('AK8PFchs')
+#            ),
+#      cms.PSet(#puppi
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_%s'%tagNamePuppi),
+#            label  = cms.untracked.string('AK4PFPuppi')
+#            ),
+#      cms.PSet( ## AK8 puppi
+#            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_%s'%re.sub('AK4','AK8',tagNamePuppi)),
+#            label  = cms.untracked.string('AK8PFPuppi')
+#            ),
+#      ## here you add as many jet types as you need
+#      ## note that the tag name is specific for the particular sqlite file 
+#      ), 
+#      connect = connectString
+#     # uncomment above tag lines and this comment to use MC JEC
+#)
+### add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
+#process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+# end JH - JEC, if using from sqlite file instead of global tag.                                                                              
 
-updateJetCollection(
-   process,
-   jetSource = cms.InputTag('slimmedJets'),
-   labelName = 'UpdatedJEC',
-   jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'NONE')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
-)
+### ADD SOME NEW JET COLLECTIONS                                                                                                              
+# New (March 8, 2019) - to recover ak8 CHS jets with 2017 MiniAOD
+#################################
+  ###  JET TOOLBOX FOR CHS ###
+#################################
+# AK R=0.8 jets from CHS inputs with basic grooming, W tagging, and top tagging                                                            
+from JMEAnalysis.JetToolbox.jetToolbox_cff import *
 
+jetToolbox( process, 'ak8', 'ak8JetSubs', 'noOutput',
+                PUMethod='CHS',
+                addPruning=True, addSoftDrop=False ,           # add basic grooming                                                            
+                addTrimming=False, addFiltering=False,
+                addSoftDropSubjets=False,
+                addNsub=True, maxTau=4,                       # add Nsubjettiness tau1, tau2, tau3, tau4                                      
+                # added L1FastJet on top of the example config file
+                JETCorrPayload = 'AK8PFchs', JETCorrLevels = ['L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual']
+                )
+
+#################################
+    ###  RECORRECTING JETS ###
+#################################
+     # New from Ksenia+Finn - update jet corrections
+#from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+#updateJetCollection(
+#                   process,
+#                   jetSource = cms.InputTag('slimmedJetsAK8'),
+#                   labelName = 'UpdatedJECAK8',
+#                   jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'NONE')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+#                    )
+################################
+# DEFINE COLLECTION TO BE PASSED FURTHER
+###############################
+#if YEAR == 2017:
+COLLECTIONTOSMEAR="selectedPatJetsAK8PFCHS" ## toolbox output
+#if YEAR == 2016:
+#     COLLECTIONTOSMEAR="updatedPatJetsUpdatedJECAK8" ## recorrected miniAOD collection
+
+#################################
+     ###  JER SMEARING ###
+#################################
+from RecoMET.METProducers.METSigParams_cfi import *
+process.slimmedAK8JetsSmeared = cms.EDProducer('SmearedPATJetProducer',
+        src = cms.InputTag(COLLECTIONTOSMEAR),
+        enabled = cms.bool(DOSMEARING),
+        rho = cms.InputTag("fixedGridRhoFastjetAll"),
+        algo = cms.string('AK8PFchs'),
+        algopt = cms.string('AK8PFchs_pt'),
+
+        genJets = cms.InputTag('slimmedGenJets'),
+        dRMax = cms.double(0.2),
+        dPtMaxFactor = cms.double(3),
+        seed = cms.uint32(37428479),
+        debug = cms.untracked.bool(False),
+    # Systematic variation
+    # 0: Nominal
+    # -1: -1 sigma (down variation)
+    # 1: +1 sigma (up variation)
+    variation = cms.int32(0)  # If not specified, default to 0
+        )
+
+
+#################################
+        ###  JET ID  ###
+#################################
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 process.slimmedJetsAK8JetId = cms.EDFilter("PFJetIDSelectionFunctorFilter",
                                            filterParams = pfJetIDSelector.clone(),
                                            #src = cms.InputTag("slimmedJetsAK8"),
-                                           src = cms.InputTag("updatedPatJetsUpdatedJECAK8"),
+                                           src = cms.InputTag("slimmedAK8JetsSmeared"),
                                            filter = cms.bool(True)
                                            )
-from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
-process.slimmedJetsJetId = cms.EDFilter("PFJetIDSelectionFunctorFilter",
-                                           filterParams = pfJetIDSelector.clone(),
-                                           #src = cms.InputTag("slimmedJets"),
-                                           src = cms.InputTag("updatedPatJetsUpdatedJEC"), 
-                                           filter = cms.bool(True)
+                                           
+#################################
 
-                                           )
 
+# If using full proton reco (2016 style)
+#process.load("RecoCTPPS.ProtonReconstruction.year_2016.ctppsProtonReconstruction_cfi")
+
+# If using full proton reco (2017 style) 
+process.load("RecoCTPPS.ProtonReconstruction.year_2017_OF.ctppsProtonReconstructionOF_cfi")
+#from RecoCTPPS.ProtonReconstruction.year_2017_OF.ctppsProtonReconstructionOF_cfi import *
+#process.ctppsProtonReconstructionOF = ctppsProtonReconstructionOF
+#process.ctppsProtonReconstructionOF.alignmentFiles = cms.vstring("RecoCTPPS/ProtonReconstruction/data/alignment/2017_preTS2/collect_alignments_$alignment.out")
 
 # If using full proton reco (2016 style)
 #process.load("RecoCTPPS.ProtonReconstruction.year_2016.ctppsProtonReconstruction_cfi")
@@ -224,6 +349,23 @@ if MC:
         process.demo.jetAK8CHSCollection = cms.InputTag("slimmedJetsAK8JetId")
 
         process.demo.recoProtonsCollection = cms.InputTag("ctppsProtonReconstruction")
+<<<<<<< HEAD
+
+    if YEAR == 2017:
+        process.demo.dataPileupFile = cms.string("PUHistos_data_2017.root")
+        process.demo.jetAK8CHSCollection = cms.InputTag("slimmedJetsAK8JetId")
+        
+        # Standard 2017 central production input distribution                                                                                        
+        process.demo.mcPileupFile = cms.string("PUHistos_mc_2017.root")                                                                              
+
+        # Special cases for buggy datasets in 2017 MC                                                                                                
+        #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_QCDPt300to470.root")
+        #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_QCDPt600to800.root")
+        #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_QCDPt1000to1400.root")                                                         
+        #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_Wjets.root")
+        #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_Zjets.root")     
+
+=======
 
     if YEAR == 2017:
         process.demo.dataPileupFile = cms.string("PUHistos_data_2017.root")
@@ -239,6 +381,7 @@ if MC:
         #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_Wjets.root")
         #    process.demo.mcPileupFile = cms.string("PUHistos_mc_2017_Zjets.root")     
 
+>>>>>>> 535c935a54c61db271a5a935bdba13963aa11c57
         process.demo.recoProtonsCollection = cms.InputTag("ctppsProtonReconstructionOFDB")
 
 else:
@@ -247,22 +390,36 @@ else:
         process.demo.jetAK8CHSCollection = cms.InputTag("slimmedJetsAK8JetId")
         process.demo.recoProtonsCollection = cms.InputTag("ctppsProtonReconstruction")
     if YEAR == 2017:
+<<<<<<< HEAD
+#        process.demo.jetAK8CHSCollection = cms.InputTag("selectedPatJetsAK8PFCHS")
+#        process.demo.jetAK8CHSCollection = cms.InputTag("slimmedAK8JetsSmearedJetID")
+        process.demo.jetAK8CHSCollection = cms.InputTag("slimmedJetsAK8JetId")
+
+=======
         process.demo.jetAK8CHSCollection = cms.InputTag("selectedPatJetsAK8PFCHS")
+>>>>>>> 535c935a54c61db271a5a935bdba13963aa11c57
         process.demo.recoProtonsCollection = cms.InputTag("ctppsProtonReconstructionOFDB")
 
 process.demo.year = cms.int32(YEAR)
 process.demo.era = cms.string(ERA)
 
-process.p = cms.Path(process.hltFilter * 
-                     process.patJetCorrFactorsUpdatedJEC * 
-                     process.updatedPatJetsUpdatedJEC *
-                     process.patJetCorrFactorsUpdatedJECAK8 *
-                     process.updatedPatJetsUpdatedJECAK8 *
+
+
+process.p = cms.Path(process.hltFilter *           
+                    # process.patJetCorrFactorsUpdatedJECAK8 *
+                    # process.updatedPatJetsUpdatedJECAK8 * 
+                     process.slimmedAK8JetsSmeared *
                      process.slimmedJetsAK8JetId *
+<<<<<<< HEAD
+                  #process.ctppsProtonReconstruction *
+                  #  process.ctppsProtonReconstructionOF * 
+                 #     process.ctppsProtonReconstructionOFDB *
+=======
                      process.slimmedJetsJetId *
 ##                     process.ctppsProtonReconstruction *
 ##                     process.ctppsProtonReconstructionOF * 
 #                     process.ctppsProtonReconstructionOFDB *
+>>>>>>> 535c935a54c61db271a5a935bdba13963aa11c57
                      process.demo)
 
 print process.dumpPython()
