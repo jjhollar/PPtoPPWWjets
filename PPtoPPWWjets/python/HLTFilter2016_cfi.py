77b7e281e15ac007d55dc2d7c90abb8271a03321
Jonathan <jjhollar@cern.ch>
2021-05-20 15:51:05 +0200

    Fix 2016 HLT cfi for missing underscores

--- PPtoPPWWjets/python/HLTFilter2016_cfi.py
+++ PPtoPPWWjets/python/HLTFilter2016_cfi.py
@@ -5,15 +5,15 @@ import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 hltFilter = copy.deepcopy(hltHighLevel)
 hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-hltFilter.HLTPaths = ['HLT PFHT800_v*',
-                      'HLT PFHT900_v*',
-                      'HLT PFJet450_v*',
-                      'HLT PFJet500_v*',
+hltFilter.HLTPaths = ['HLT_PFHT800_v*',
+                      'HLT_PFHT900_v*',
+                      'HLT_PFJet450_v*',
+                      'HLT_PFJet500_v*',
                       'HLT_AK8PFJet450_v*',
                       'HLT_AK8PFJet500_v*',
-                      'HLT AK8PFJet360 TrimMass30_v*',
-                      'HLT AK8PFHT700 TrimR0p1PT0p03Mass50_v*',
-                      'HLT PFHT650 WideJetMJJ900DEtaJJ1p5_v*',
-                      'HLT PFHT650 WideJetMJJ950DEtaJJ1p5_v*']
+                      'HLT_AK8PFJet360_TrimMass30_v*',
+                      'HLT_AK8PFHT700_TrimR0p1PT0p03Mass50_v*',
+                      'HLT_PFHT650_WideJetMJJ900DEtaJJ1p5_v*',
+                      'HLT_PFHT650_WideJetMJJ950DEtaJJ1p5_v*']
 
 hltFilter.throw = cms.bool(False)
